be218575400bf677660d35ad1bb961bee9340619
AndreOliveiraBrt <93344694+AndreOliveiraBrt@users.noreply.github.com>
2022-09-21 11:06:30 -0300

    feat: Definição Classe ClienteDTO

--- niveisteste/src/lojinha/dto/ClienteDto.java
+++ niveisteste/src/lojinha/dto/ClienteDto.java
@@ -1,4 +1,40 @@
 package lojinha.dto;
 
 public class ClienteDto {
+    private int clienteId;
+    private String clienteNone;
+    private boolean clienteVip;
+
+    public ClienteDto(){
+
+    }
+    public ClienteDto(int clienteId, String clienteNone, boolean clienteVip){
+        this.clienteId=clienteId;
+        this.clienteNone=clienteNone;
+        this.clienteVip=clienteVip;
+    }
+
+    public int getClienteId() {
+        return clienteId;
+    }
+
+    public void setClienteId(int clienteId) {
+        this.clienteId = clienteId;
+    }
+
+    public String getClienteNone() {
+        return clienteNone;
+    }
+
+    public void setClienteNone(String clienteNone) {
+        this.clienteNone = clienteNone;
+    }
+
+    public boolean isClienteVip() {
+        return clienteVip;
+    }
+
+    public void setClienteVip(boolean clienteVip) {
+        this.clienteVip = clienteVip;
+    }
 }
